03417b30dbc8b999dbfb26a4886cd8946e9b6059
Nigel Yeo <nigelyyz@yahoo.com.sg>
2022-07-14 16:22:51 +0800

    Added React components Airline and Airlines and routes

---
+++ app/javascript/components/Airlines/Airlines.js
@@ -0,0 +1,17 @@
+import React, {useState, useEffect} from 'react'
+
+const Airlines = () => {
+  const [airlines, setAirlines] = useState([])
+
+  useEffect(() => {
+    // Get all of our airlines from api
+    //Update airlines in our state
+  })
+
+
+  return (
+    <div>This is the Airlines#index view for our app.</div>
+  )
+}
+
+export default Airlines
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'pages#index'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :airlines, param: :slug
+      resources :reviews, only: [:create, :destroy]
+    end
+  end
+
+  # Handles Routing to React components without interfering with Rails Routes for our api
+  get '*path', to: 'pages#index', via: :all
 end
